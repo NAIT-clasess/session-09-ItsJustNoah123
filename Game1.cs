9d629ca8047320072bae418d81e86b33c145a9ee
ItsJustNoah123 <noahhemmelgarn204@hotmail.com>
2026-02-03 14:20:25 -0700

    Add ball movement and rename screen constants
    Rename screen and play-area constants for consistency and fix typos. Add ball movement state (position, direction, speed) and a play-area bounding box. Initialize preferred back buffer size, initial ball position/direction and speed (set to 200) in Initialize, and apply graphics changes. Update Update to move the ball using direction * speed * elapsed time. Update Draw to use the renamed screen constants, compute the ball Rectangle, and draw the ball texture.

--- Game1.cs
+++ Game1.cs
@@ -9,17 +9,19 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
     // Screen
-    private const int PerferredScreenWidth = 750;
-    private const int PerferredScreenHeight = 450;
+    private const int _preferredScreenWidth = 750;
+    private const int _preferredScreenHeight = 450;
     // Play Area
-    private const int _playeAreaEdgeLineWidth = 12;
+    private const int _playAreaEdgeLineWidth = 12;
     // Ball
     private const int _ballWidthAndHeight = 21;
     // Textures
     private Texture2D _backgroundTexture, _ballTexture;
+    // Ball movement
+    private float _ballSpeed = 10;
+    private Vector2 _ballPosition, _ballDirection;
 
-
-
+    private Rectangle _playAreaBoundingBox;
 
     public Game1()
     {
@@ -31,9 +33,18 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-    
-        _graphics.PreferredBackBufferWidth = PerferredScreenWidth;
-        _graphics.PreferredBackBufferHeight = PerferredScreenHeight;
+        // Screen Size
+        _graphics.PreferredBackBufferWidth = _preferredScreenWidth;
+        _graphics.PreferredBackBufferHeight = _preferredScreenHeight;
+        // Initial Ball Position and Direction
+        _ballPosition.X = 150;
+        _ballPosition.Y = 195;
+        _ballSpeed = 200;
+        _ballDirection.X = 1;
+        _ballDirection.Y = -1;
+
+        _playAreaBoundingBox = new(0, 0, _preferredScreenWidth, _preferredScreenHeight);
+        // Apply Graphics changes
         _graphics.ApplyChanges();
         base.Initialize();
     }
@@ -53,6 +64,8 @@ public class Game1 : Game
             Exit();
 
         // TODO: Add your update logic here
+            _ballPosition += _ballDirection * _ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        
 
         base.Update(gameTime);
     }
@@ -63,7 +76,11 @@ public class Game1 : Game
 
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_backgroundTexture, new Rectangle(0, 0, PerferredScreenWidth, PerferredScreenHeight), Color.White);
+        
+        _spriteBatch.Draw(_backgroundTexture, new Rectangle(0, 0, _preferredScreenWidth, _preferredScreenHeight), Color.White);
+        var ballRect = new Rectangle((int)_ballPosition.X, (int)_ballPosition.Y, _ballWidthAndHeight, _ballWidthAndHeight);
+        
+        _spriteBatch.Draw(_ballTexture, ballRect, Color.White);
         _spriteBatch.End();
         base.Draw(gameTime);
     }
